796b50e6799a6eb3fc80c36cc554e0923fe600cd
germanespinosa <germanespinosa@gmail.com>
2019-11-14 13:56:05 -0800

    new file:   lib/catch/CMakeLists.txt 	new file:   lib/catch/src/catch-main.cpp 	new file:   lib/catch/src/catch.h

---
+++ lib/catch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cs211/target/lib/catch
+# Build directory: /home/cs211/target/lib/catch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
