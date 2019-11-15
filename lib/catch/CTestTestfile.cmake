1b83386241b8d52adaa0d075fbd9e92aedb5cdb4
germanespinosa <germanespinosa@gmail.com>
2019-11-15 11:53:40 -0800

    deleted:    lib/catch/CMakeFiles/CMakeDirectoryInformation.cmake 	deleted:    lib/catch/CMakeFiles/catch.dir/CXX.includecache 	deleted:    lib/catch/CMakeFiles/catch.dir/DependInfo.cmake 	deleted:    lib/catch/CMakeFiles/catch.dir/build.make 	deleted:    lib/catch/CMakeFiles/catch.dir/cmake_clean.cmake 	deleted:    lib/catch/CMakeFiles/catch.dir/cmake_clean_target.cmake 	deleted:    lib/catch/CMakeFiles/catch.dir/depend.internal 	deleted:    lib/catch/CMakeFiles/catch.dir/depend.make 	deleted:    lib/catch/CMakeFiles/catch.dir/flags.make 	deleted:    lib/catch/CMakeFiles/catch.dir/link.txt 	deleted:    lib/catch/CMakeFiles/catch.dir/progress.make 	deleted:    lib/catch/CMakeFiles/catch.dir/src/catch-main.cpp.o 	deleted:    lib/catch/CMakeFiles/progress.marks 	deleted:    lib/catch/CTestTestfile.cmake 	deleted:    lib/catch/Makefile 	deleted:    lib/catch/cmake_install.cmake 	deleted:    lib/catch/libcatch.a

--- lib/catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs211/target/lib/catch
-# Build directory: /home/cs211/target/lib/catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
